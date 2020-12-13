95bd9440432a041ea0c264e0a44fa9ab0393251e
Jcodevisible <jameelahr2002@gmail.com>
2020-12-13 15:11:21 -0500

    Restructured call client capability

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.css';
-import { CallClient, CallAgent} from "@azure/communication-calling";
+import { CallClient} from "@azure/communication-calling";
 import { AzureCommunicationUserCredential } from '@azure/communication-common';
 import AppSettings from './appsettings.json';
 
@@ -20,11 +18,11 @@ class App extends React.Component {
     }
     ////////////////////Issues User Token with scopes ////////////////////////////
     issueToken = async () => {
-        console.log("Azure Communication Services - User Access Tokens Quickstart")
+        console.log("Welcome to Azure Communication Services")
         // Place your connection string in the appsettings.json file
         const connectionString = AppSettings.ResourceConnectionString;
-        
-        //Uncomment the below console log line to check 
+
+        //Uncomment the below console log line to check
         //if your connectionstring was read from the appsettings.json file
         //console.log(connectionString)
 
@@ -33,27 +31,31 @@ class App extends React.Component {
         //Use the below line to display the identityClient in the console
         // console.log(identityClient)
 
+        //Note: userResponse is the identityResponse in the Quickstart here: https://docs.microsoft.com/en-us/azure/communication-services/quickstarts/access-tokens?pivots=programming-language-javascript
         let userResponse = await identityClient.createUser();
         console.log(`\nCreated a user with ID: ${userResponse.communicationUserId}`);
 
-        // Issue an access token with multiple scopes for a new user
-        let tokenResponse = await identityClient.issueToken(userResponse, ["voip", "chat", "pstn", "joinRoom"]);
-
+        //Issue tokens 
+        let  tokenResponse = await identityClient.issueToken(userResponse, ["voip", "chat", "pstn", "joinRoom"]);
+        
 
         const { token, expiresOn } = tokenResponse;
 
         // console.log(`\nIssued a token that expires at ${expiresOn}:`);
         // console.log(token);
 
+        /////This sets the state of each object that I want to show on the screen so I can refer to it later//////
         this.setState({
             newtoken: (`\nIssued a token that expires at ${expiresOn}`),
             newuser: (`\nCongrats! You've provisioned a new ACS user to use the ACS SDK.  The identity is: ${userResponse.communicationUserId}:`),
             issuedtoken: token
-        });
+        }); 
+        
+        
 
-        this.callClientInit();
-
-    };
+    }
+   
+  
 
     ////////////////////Initializes the Azure Calling SDK ////////////////////////////
     callClientInit = async () => {
@@ -65,9 +67,10 @@ class App extends React.Component {
 
         // Authenticate the client
         const callClient = new CallClient();
-        const tokenCredential = new AzureCommunicationUserCredential(this.state.issuedtoken);  
+        const tokenCredential = new AzureCommunicationUserCredential(this.state.issuedtoken);
         let callAgent;
-      
+
+        
 
         // CallClient is the entrypoint for the SDK. Use it to
         // to instantiate a CallClient and to access the DeviceManager
@@ -94,7 +97,6 @@ class App extends React.Component {
         hangUpButton.addEventListener("click", () => {
             // end the current call
             call.hangUp({ forEveryone: true });
-
             // toggle button states
             hangUpButton.disabled = true;
             callButton.disabled = false;
@@ -103,101 +105,104 @@ class App extends React.Component {
 
     };
 
+////////Creates a new Group ID ////////
+
+
+
+
+
+
+
+
+
 
 
     render() {
         return (
-            <div className="App">
-                <header className="header sticky-top sticky navbar navbar-default ">
-                    <div class="container ">
-                        <div class="row">
-                            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <div class="page-caption">
-                                    <h2 class="page-title">The example below shows how to use the Azure.Communication SDKs.</h2>
-                                </div>
 
-                            </div>
-                        </div>
-                    </div>
-                </header>
+
+            <div className="App">
+                 <header>
+                    <h1 class="intro">
+                   <h2 class="page-title">The example below shows how to use the Azure.Communication SDKs.</h2>         
+                    </h1>
+                </header> 
 
                 <body>
-                    <Container>
-                        <Jumbotron>
+                    <div class="jumbotron container">
+                        <h1 class="display-4">Hello, ACS User!</h1>
+                        <p class="lead">This is a simple application for user authentication, voice and video calling, chat, and SMS features utilizing the Azure Communication Services SDKs.</p>
+                        <hr class="my-4"></hr>
+                        <p>Click the button below to issue a user token.</p>
+                        <p class="lead">
+                        <Button onClick={this.issueToken} > Issue User Identity and Token</Button>
+                        <div class="utokenModal container">{this.state.newtoken} <br></br> {this.state.newuser}</div>
+                        </p>
+                    </div>
 
-                            <p >  Click the button below to issue a user token.</p>
-                            {/*  <div class="utokenModal container">{this.state.tokencode}</div>
-                            <Button color="primary" className="px-4" onClick={this.showCode}>Show Code </Button> <span className="px-4"></span> */}
-                            <Button onClick={this.issueToken} > Provision new user and Initialize SDK</Button>
-                            <div class="utokenModal container">{this.state.newtoken} <br></br> {this.state.newuser}</div>
+                    <div class="jumbotron container">
+                        <h2 class="display-4">Create a Call Client</h2> 
+                    <p class="lead">The Call Client instantiates the Call Agent which is used to start a call.</p>
+                    &nbsp;
+                    <Button onClick={this.callClientInit}> Create a Call Client</Button>
+                      </div>
 
-                        </Jumbotron>
-                    </Container>
-
-                    <div class="container">
-                        <h3>Call, Chat, and Send a SMS Below</h3>
-                        <div class="panel-group">
-                            <div class="panel panel-default">
-                                <div class="panel-heading"><label>Placing and receiving calls</label> </div>
-                                <div class="panel-body"> Having provisioned an ACS user identity and initialized the SDK, you are now ready to place a call.<br></br>
-                                    You can make an outbound VOIP call by providing a user ID in the text field and clicking the Start Call button. <br></br>Calling <label>8:echo123</label> 
-                                     connects you with an echo bot, this is great for getting started and verifying your audio devices are working.</div>
-                                <div class="panel-body"> Input a destination user id, and place the call.</div>
-                                <input class="InputBox"
+                    <div class="jumbotron container">
+                        <h2 class="display-4">Place a VOIP Call</h2>
+                        <div class="panel-body lead"> Having provisioned an ACS user identity and initialized the SDK, you are now ready to place a call.
+                         You can make an outbound VOIP call by creating a call client, providing a user ID in the text field and clicking the Place Call button. <br></br><br></br>Calling <b>8:echo123 </b>connects you with an echo bot, this is great for getting started and verifying your audio devices are working.</div>
+                        <hr class="my-4"/>
+                        <p>Input a destination user id, and place the call.</p>
+                        
+                            <input class="InputBox"
                                     id="callee-id-input"
                                     type="text"
-                                    placeholder="Who would you like to call?"/> 
-                                
+                                    placeholder="Who would you like to call?"/>
+
                                 <div>
                                     <button id="call-button" type="button" disabled="true">
-                                        Start Call
+                                        Place Call
                                       </button>
                                       &nbsp;
                                       <button id="hang-up-button" type="button" disabled="true">
-                                       Hang Up
+                                       End Call &amp; Dispose Call Client
                                       </button>
+                                      
                                 </div>
-                                <script src="./bundle.js"></script>
+                       
+                        <script src="./bundle.js"></script>
+                    </div>
 
-                                 </div>
 
+                    <div class="jumbotron container">
+                        <h2 class="display-4">Create a Group Call</h2>
+                        <p class="lead">
+                        <Button onClick={this.getGroupId} > Create a Group ID</Button>
+                        </p>
+                        <div class="panel-body lead"> After creating your Group ID, open another browser window and place the group id url into the address bar.  Have both windows join the same group ID.</div>
+                        <hr class="my-4"/>
+                        <p>Input the Group Id below and click 'Create A Group Call'.</p>                      
+                            <input class="InputBox"
+                                    id="Group-id-input"
+                                    type="text"
+                                    placeholder="What is the Group ID?"/>
 
-                            
-                            <div class="panel panel-default">
-                                <div class="panel-heading">Join a Group Call</div>
-                                <div class="panel-body"> Open another browser window and for both windows join the same group ID.</div>
-                                <Button onClick='' > Join  Group </Button>
-                                <div class="panel-body">
-                                    <label for="GroupId">Group Id:</label>
-
+                                <div>
+                                    <button id="call-button" type="button" disabled="true">
+                                        Create A Group Call
+                                      </button>
+                                      
                                 </div>
-                            </div>
-                         
-                            
-                            <div class="panel panel-info">
+                       
+                    </div>
+
+                            {/* <div class="panel panel-info">
                                 <div class="panel-heading">Join a Teams Meeting</div>
                                 <div class="panel-body"> Enter a Teams meeting link or a Teams meeting coordinates</div>
                                 <Button onClick='' > Join Teams Meeting</Button>
                                 <div class="panel-body"> Meeting Link</div>
                             </div>
-                {/*
-                            <div class="panel panel-default">
-                                <div class="panel-heading">Start A Chat</div>
-                                <div class="panel-body"> Content</div>
-                                <Button onClick='' > Start A Chat</Button>
-                                <div class="panel-body"> Content</div>
-                            </div>
-
-                            <div class="panel panel-default">
-                                <div class="panel-heading">Send a SMS Message</div>
-                                <div class="panel-body"> Content</div>
-                                <Button onClick='' > Send SMS Message</Button>
-                                <div class="panel-body"> Content</div>
-                            </div>
-                */}
-
-                        </div>
-                    </div>
+                         */}
 
                 </body>
 
@@ -205,12 +210,11 @@ class App extends React.Component {
         );
     }
 
-    //issueToken().catch((error) => {
+ 
 
-    //    console.log("Encountered and error");
-    //    console.log(error);
-    //});
 }
+
+
 
 
 
